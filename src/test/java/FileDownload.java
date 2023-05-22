39adf3cb7ef8a72cadae87992f9a8863449ee464
AnnaYedoshina <yedoshina.java@gmail.com>
2023-05-22 16:51:14 +0300

    New tests added

---
+++ src/test/java/FileDownload.java
@@ -0,0 +1,53 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.time.Duration;
+
+public class FileDownload {
+    private static final String URL = "http://the-internet.herokuapp.com/download";
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    public static boolean isFileExists(File file) {
+        return file.isFile();
+    }
+
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(URL);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+
+    }
+
+    @Test
+    public void fileDownloadTest() {
+        WebElement link = driver.findElement(By.cssSelector("//a[text() = 'text.txt']"));
+        link.click();
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("download.default_directory", System.getProperty("user.dir") + "/target/downloads");
+        String filePath = "System.getProperty(user.dir) + /target/downloads";
+        File file = new File(filePath);
+
+        if (isFileExists(file)) {
+            System.out.println("File exists!!");
+        } else {
+            System.out.println("File doesn't exist or program doesn't have access " +
+                    "to the file");
+        }
+    }
+}
---
+++ src/test/java/Frames.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Frames {
+    private static final String URL = "http://the-internet.herokuapp.com/frames";
+    private WebDriver driver;
+    @BeforeClass
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(URL);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+
+    }
+
+    @Test
+    public void framesTest() {
+        driver.findElement(By.cssSelector("a[href = '/iframe']")).click();
+        driver.switchTo().frame("mce_0_ifr");
+        WebElement frame = driver.findElement(By.id("tinymce"));
+        String frameText = frame.getText();
+        Assert.assertEquals(frameText, "Your content goes here.");
+
+    }
+}
